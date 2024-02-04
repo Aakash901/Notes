a50c09b07ca20e08a5ee4c28c9a56b3e9d5e4c15
Aakash Singh <aakash.kr.singh0102@gmail.com>
2024-02-05 00:20:58 +0530

    all file upload

--- app/src/main/java/com/example/notes/ui/fragment/HomeFragment.java
+++ app/src/main/java/com/example/notes/ui/fragment/HomeFragment.java
@@ -131,7 +131,7 @@ public class HomeFragment extends Fragment {
                 fragmentTransaction.commit();
             }
         });
-
+//
 
         userPic.setOnClickListener(new View.OnClickListener() {
             @Override
